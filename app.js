2b5c049e4e8fd77a2ac96715b13df1887558eb5e
Abhijit Apotikar <abhijit.apotikar156@gmail.com>
2024-12-01 17:03:17 +0530

    fix: add route for the route URL

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-app.get('/categories', (req, res)=>{
+app.get('/', (req, res)=>{
 	const responseData = {message : 'Hello from pc_marketplace!:)'};
 	res.status(200).json(responseData);
 });
