b3ad5c5992ec700efba905c477123c7d18425562
Abhijit Apotikar <abhijit.apotikar156@gmail.com>
2024-12-02 11:34:57 +0530

    update: add cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 4000;
+
+app.use(cors());
 
 app.get('/', (req, res)=>{
 	const responseData = {message : 'Hello from pc_marketplace!:)'};
